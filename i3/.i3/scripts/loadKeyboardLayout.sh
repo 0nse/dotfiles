9f61a5c5f483409308eb12cd4c8edbebb0580646
0nse <0nse@users.noreply.github.com>
2015-07-22 16:38:08 +0200

    + Few steps closer to loading keyboard layout on pluggin in pok3r. However, it still won't work. The script is triggered and notifications are sent.

--- i3/.i3/scripts/loadKeyboardLayout.sh
+++ i3/.i3/scripts/loadKeyboardLayout.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
+#xorgVars=`psgrep -a Xorg`
+export DISPLAY=:0
+export HOME="/home/`users | awk '{print $1}'`"
+# Obtain the greeter cookie:
+export XAUTHORITY=`ls /tmp/serverauth.*`
+
 usermodmap=$HOME/.Xmodmap
 
 sleep 1 # sleep to allow udev finish loading the keyboard first
-setxkbmap -layout us -variant altgr-intl
+setxkbmap -layout us -variant altgr-intl -display $DISPLAY
 
 if [ -f "$usermodmap" ]; then
-    xmodmap "$usermodmap"
+    xmodmap -display $DISPLAY "$usermodmap"
 fi
 
 notify-send "Enjoy your feeling." -u low -t 500
