9ecdaf05a041f0939f1c49c2ae574a54aecf038c
0nse <0nse@users.noreply.github.com>
2015-10-27 20:57:26 +0100

    [i3] / Fix caffeine.

--- i3/.i3/scripts/caffeine.sh
+++ i3/.i3/scripts/caffeine.sh
@@ -7,12 +7,12 @@ if [[ $1 == "on" ]]; then
   binaryValue=0
   xsetValue="+"
   pkill xflux-bin
-  $1="off"
+  state="off"
 elif [[ $1 == "off" ]]; then
   binaryValue=10
   xsetValue="-"
   xflux &
-  $1="on"
+  state="on"
 else
   echo "Allowed values are 'on' and 'off'."
   exit 1
@@ -20,11 +20,11 @@ fi
 
 setterm --blank ${binaryValue} # switch screen blanking
 setterm --powerdown ${binaryValue} # switch turning screen off
-setterm --powersave $1 # switch power saving
-xset s $1 # switch X fallback screensaver
+setterm --powersave ${state} # switch power saving
+xset s ${state} # switch X fallback screensaver
 xset ${xsetValue}dpms # switch DPMS
 
-if [[ $1 == "on" ]]; then
+if [[ $1 == "off" ]]; then
   textMessage="Slumber pills: The effect has worn off."
 else
   textMessage="Key induced slumber."
