5568230d156a1117e2d9b7b3f0e73286674f0ff4
0nse <0nse@users.noreply.github.com>
2016-01-27 20:32:13 +0100

    [bash] / Cleanup. [bash] / Load programs in xinitrc instead of i3. [bash] / Use unclutter-xfixes instad of unclutter. [bash] / Use redshift instead of the xflux binary. [i3] / Fix PA volume change sound not playing. [i3] / Use different file for VNC password.

--- i3/.i3/scripts/loadKeyboardLayout.sh
+++ i3/.i3/scripts/loadKeyboardLayout.sh
@@ -5,13 +5,13 @@ export HOME="/home/`users | awk '{print $1}'`"
 # Obtain the greeter cookie:
 export XAUTHORITY=`ls /tmp/serverauth.*`
 
-usermodmap=$HOME/.Xmodmap
+USER_MODMAP=$HOME/.Xmodmap
 
 sleep 1 # sleep to allow udev finish loading the keyboard first
-setxkbmap -layout us -variant altgr-intl -display $DISPLAY
+setxkbmap -layout us -variant altgr-intl -display "${DISPLAY}"
 
-if [ -f "$usermodmap" ]; then
-    xmodmap -display $DISPLAY "$usermodmap"
+if [ -f "${USER_MODMAP}" ]; then
+    xmodmap -display "${DISPLAY}" "${USER_MODMAP}"
 fi
 
 notify-send "Enjoy your feeling." -u low -t 500
