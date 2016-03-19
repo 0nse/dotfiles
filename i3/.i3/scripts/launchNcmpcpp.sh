14694b22848c4d86c5e4c2c998e90a7e762af795
0nse <0nse@users.noreply.github.com>
2016-03-19 15:26:26 +0100

    [i3] / Simplified launchNcmpcpp drastically.

--- i3/.i3/scripts/launchNcmpcpp.sh
+++ i3/.i3/scripts/launchNcmpcpp.sh
@@ -1,26 +1,9 @@
 #!/bin/bash
-# This script requires xdotool to be installed. It launches ncmpcpp and floats
-# the window when necessary (that is, when the window size is too small for
-# ncmpcpp to start).
-
-ACTIVE_WINDOW_PID=-1
-
-function getPID {
-  focused_window_id=$(xdotool getwindowfocus)
-  active_window_id=$(xdotool getactivewindow)
-  ACTIVE_WINDOW_PID=$(xdotool getwindowpid "$active_window_id")
-}
-
-function setTitle {
-  title="${1}"
-  xdotool search --onlyvisible --pid "${ACTIVE_WINDOW_PID}" \
-                 --name "\a\b\c" set_window --name "${title}"
-}
+# This script launches ncmpcpp and floats the window when necessary (that is,
+# when the window size is too small for ncmpcpp to start).
 
 ncmpcpp
 if [[ "$?" -ne 0 ]]; then
-  getPID
-  # this only works with a dedicated i3 for_window rule:
-  setTitle "i3:FLOAT"
+  i3 floating toggle
   ncmpcpp
 fi
