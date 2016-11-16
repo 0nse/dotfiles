03404ca6c1c94a2f26b5f6d2456a00092cebae87
0nse <0nse@users.noreply.github.com>
2016-11-16 20:07:36 +0100

    [i3] Launch ncmpcpp regularly because st handles fonts well.

--- i3/.i3/scripts/launchNcmpcpp.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# This script launches ncmpcpp and floats the window when necessary (that is,
-# when the window size is too small for ncmpcpp to start).
-
-ncmpcpp
-if [[ "$?" -ne 0 ]]; then
-  i3 floating toggle
-  ncmpcpp
-fi
