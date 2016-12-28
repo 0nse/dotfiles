410e4b026cc376d94e03f390cf12b7a1b6ed3cae
0nse <0nse@users.noreply.github.com>
2016-12-28 19:39:48 +0100

    [bash] Update aliases.

--- bash/.bash/alias.bash
+++ bash/.bash/alias.bash
@@ -1,4 +1,6 @@
-alias vim=nvim
 alias ag='ag --nobreak --color'
+alias diff='colordiff'
+alias ls='ls --group-directories-first --color'
 alias mopidy='/usr/bin/mopidy --config /var/lib/mopidy/mopidy.conf'
 alias nnvim='git number -c nvim'
+alias vim=nvim
