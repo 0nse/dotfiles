2b43a106a8103b8153e33f233d76b608374da4da
0nse <0nse@users.noreply.github.com>
2017-01-12 20:21:12 +0100

    [bash] Workaround for QT 5.7 ignoring QT_STYLE_OVERRIDE.

--- bash/.bash/exports.bash
+++ bash/.bash/exports.bash
@@ -2,6 +2,7 @@ export EDITOR="nvim"
 export TEXMFHOME=$HOME/.texmf-local
 # make QT5 applications use GTK theme:
 export QT_STYLE_OVERRIDE=GTK+
+export QT_QPA_PLATFORMTHEME=gnome
 # enable video acceleration for radeon driver:
 export LIBVA_DRIVER_NAME=vdpau
 export VDPAU_DRIVER=r600
