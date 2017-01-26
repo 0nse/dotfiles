46fcab964ace4b619f9a677c6945a487b003c8a0
0nse <0nse@users.noreply.github.com>
2017-01-26 22:33:47 +0100

    [i3]] Use polybar in exchange for conky.
    See https://github.com/jaagr/polybar/

---
+++ i3/.i3/scripts/polybar.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+# This script was taken from the polybar Wiki
+# <https://github.com/jaagr/polybar/wiki>
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -x polybar >/dev/null; do sleep 1; done
+
+# Launch bar1 and bar2
+polybar main &
