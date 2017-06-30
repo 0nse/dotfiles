43043362aa62c3658a36bb4003e325a34b0d3e62
0nse <0nse@users.noreply.github.com>
2017-06-30 22:06:36 +0200

    [bash] Update mount path.

--- bash/.bash/scripts/mount.bash
+++ bash/.bash/scripts/mount.bash
@@ -1,13 +1,13 @@
 function mountEhdd {
   sudo cryptsetup luksOpen /dev/sdb1 ehdd
   sudo vgchange -ay /dev/ehdd
-  sudo mkdir -p /run/media/`whoami`/ehdd
+  sudo mkdir -p /media/ehdd
   sleep 1
-  sudo mount /dev/ehdd/main /run/media/`whoami`/ehdd
+  sudo mount /dev/ehdd/main /media/ehdd
 }
 
 function umountEhdd {
-  sudo umount /run/media/`whoami`/ehdd
+  sudo umount /media/ehdd
   sudo vgchange -an /dev/ehdd
   sudo cryptsetup luksClose ehdd
 }
